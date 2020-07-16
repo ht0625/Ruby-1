ba8a17caced47bd614c5a9faafbf6af2c0acffa6
hiroki tamura <hiirookii0625@gmail.com>
2020-07-16 09:03:57 +0900

    修正

--- janken.rb
+++ janken.rb
@@ -1,67 +1,39 @@
-# プレイヤー(自分)に「0 ~ 2」を入力させるロジックを書きます。
+# class Player を定義
 class Player
   def hand
-    # 「プレイヤーに 0 ~ 2を入力させる文章」を表示させます。
+    # プレイヤーに 0 ~ 2を入力させる
     puts "数字を入力してください。"
     puts "0:グー, 1:チョキ, 2:パー"
-    # 変数「input_hand」にプレイヤーが入力したものを取得して代入します。
-      # ヒント! 「getsメソッド」を使用します。
-    # input_hand =
-    input_hand = gets.to_i
-    # 「input_hand」(取得した値)が「0, 1, 2」のいずれかだとwhileから脱出させ、それ以外だと初めから繰り返させます。
-      # ヒント! 「while」と「if」を使用します。
-      # 以降の型の例に沿って実装しましょう ※実装する時場合はコメントアウトに注意しましょう
-      # while true
-        # if 条件を書く (「input_hand」(取得した値)が「0, 1, 2」のいずれかの場合だった場合)
-          # 「input_hand」(取得した値)をそのまま返します。
-            # ヒント！ 「return」を使用します。
-        # else ※条件以外の場合
-          # 「プレイヤーに 0 ~ 2を入力させる文章」を表示させます。
-          # puts "もう一度数字を入力してください。"
-          # puts "0:グー, 1:チョキ, 2:パー"
-          # 変数「input_hand」にプレイヤーが入力したものを取得して代入します。
-        # end if文のend
-      # end while文のend
+    # 変数「input_hand」にプレイヤーが入力したものを取得して代入
+    #input_hand = gets.chomp(11行目にまとめて記述)
+      # 取得した値が文字列　"0" "1" "2" でない間もしくはあいこの間、下記を繰り返し実行
       while true
-        if 0 <= input_hand && input_hand <= 2
+        input_hand = gets.chomp
+        # 取得した値が文字列　"0" "1" "2" であった場合、数値に変換 →値を返す
+        if (input_hand == "0" || input_hand == "1" || input_hand == "2")
+          input_hand = input_hand.to_i
           return input_hand
+        # 文字列　"0" "1" "2"以外が入力された場合、もう一度入力を求め、文字列として取得する
         else
           puts "もう一度数字を入力してください。"
           puts "0:グー, 1:チョキ, 2:パー"
-          input_hand = gets.to_i
+          #input_hand = gets.chomp(11行目にまとめて記述)
         end
       end
   end
 end
-# 敵が「0 ~ 2」の値をランダムで生成するロジックを書きます。
+# class Enemyを定義
 class Enemy
   def hand
-    # グー、チョキ、パーの値をランダムに出力させます。
+    # グー、チョキ、パーの値をランダムに出力
     rand(0..2)
   end
 end
-# プレイヤー(自分)が入力した「0 ~ 2」と、敵がランダムで生成した「0 ~ 2」をじゃんけんをさせて、その結果をコンソール上に出力するロジックを書きます。
+# class Jankenを定義
 class Janken
   def pon(player_hand, enemy_hand)
-    # 変数「janken」に["グー", "チョキ", "パー"]を代入します。
     janken = ["グー", "チョキ", "パー"]
-    #「相手の手は#{敵の生成した値}です。」と出力させます。
-    # puts "相手の手は#{janken[enemy_hand]}です。"
-    # Playerクラスの戻り値とEnemyクラスの戻り値からじゃんけんするロジックを作成します。
-      # 以降の型の例に沿って実装しましょう ※実装する時場合はコメントアウトに注意しましょう
-      # if 条件を書く Playerクラスの戻り値(player_hand)とEnemyクラスの戻り値(enemy_hand)の値が同じだった場合
-      # 「あいこ」を出力します。
-      # puts "あいこ"
-      #「true」を返してじゃんけんを繰り返し実行させます。
-        # ヒント！ 「return」を使用します。
-    #  elsif もしも下記の組み合わせだった場合
-      #  (player_hand == 0 && enemy_hand == 1) || (player_hand == 1 && enemy_hand == 2) || (player_hand == 2 && enemy_hand == 0)
-      # 「あなたの勝ちです」を出力します。
-      # whileを終了させる「false」を返してじゃんけんを終了させます。
-    #  else ※条件以外の場合
-      # 「あなたの負けです」を出力します。
-      # whileを終了させる「false」を返してじゃんけんを終了させます。
-    # end if文のend
+    # 勝敗判定し結果出力後、true or false の値を返す
     case (player_hand - enemy_hand + 3) % 3
     when 2
       puts "相手の手は#{janken[enemy_hand]}です。あなたの勝ちです。"
@@ -75,20 +47,10 @@ class Janken
     end
   end
 end
-# 変数「player」にPlayerをインスタンス化したものを代入します。
 player = Player.new
-# 変数「enemy」にEnemyをインスタンス化したものを代入します。
 enemy = Enemy.new
-# 変数「janken」にJankenをインスタンス化したものを代入します。
 janken = Janken.new
-# 変数「next_game」に「true」を代入します。※「next_game」が「false」にならない限り、永久にwhileで繰り返し処理を実行するためです。
 next_game = true
-# 以降の型の例に沿って実装しましょう ※実装する時場合はコメントアウトに注意しましょう。
-# while next_game
 while next_game
-# 変数「next_game」にじゃんけんを実行して返ってきた値(戻り値)を代入します。
-  # 「janken.pon(player.hand, enemy.hand)」でじゃんけんを実行しています。
   next_game = janken.pon(player.hand, enemy.hand)
-  # じゃんけんの実行結果に「false」が返ってくると、このwhileを終了します。「true」が返ってくると、その回数だけ繰り返し続けます。
-# end while文のend
 end
